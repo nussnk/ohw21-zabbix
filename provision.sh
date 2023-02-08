7418aaf4f85d7460e6ea4557b389af2f4c9c6fb9
Nikolay Kim <nuss@yandex.ru>
2023-02-08 22:31:35 +0200

    provision.sh updated

--- provision.sh
+++ provision.sh
@@ -18,19 +18,22 @@ apt install vim zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbi
 
 apt install mariadb-server mariadb-client -y
 systemctl enable --now mariadb
-
+echo 'mysql -u root create db'
 mysql -u root <<EOF
 
-  CREATE DATABASE zabbix character set utf8mb4 collate utf8mb4_bin;
   CREATE DATABASE zabbix character set utf8mb4 collate utf8mb4_bin;
   GRANT ALL PRIVILEGES ON zabbix.* TO zabbix@localhost IDENTIFIED BY 'ZabbixPass';
   FLUSH PRIVILEGES;
 EOF
 
-zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -pZabbixPass zabbix
-mysql -uroot zabbix < /vagrant/zabbix.sql
+echo 'zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -pZabbixPass zabbix'
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uroot zabbix
+echo 'mysql -uroot zabbix < /vagrant/zabbix.sql'
+zcat /vagrant/zabbix.sql.gz | mysql -uroot zabbix
 
 cp /vagrant/zabbix_server.conf /etc/zabbix/zabbix_server.conf
+cp /vagrant/zabbix.conf.php /etc/zabbix/web/zabbix.conf.php
+chown www-data:www-data /etc/zabbix/web/zabbix.conf.php
 
 apt install zabbix-frontend-php zabbix-apache-conf
 
@@ -42,8 +45,5 @@ systemctl restart apache2
 
 systemctl enable zabbix-server zabbix-agent apache2
 systemctl restart zabbix-server zabbix-agent apache2
-
-
-
-
-
+echo "now check if zabbix is running visiting your_ip/zabbix"
+echo "your ip addresses are: `ip a | grep inet | grep -v inet6| grep -v 127.0.0.1`"
